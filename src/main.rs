1993224a2dc0d94ed0ef1b0642bd59217ed1727e
Enno Boland <g@s01.de>
2020-01-29 11:30:21 +0100

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use std::convert::Infallible;
 
+use hyper::server::conn::AddrStream;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
-use hyper::server::conn::AddrStream;
-use std::net::IpAddr;
 use log::info;
-
+use std::net::IpAddr;
 
 fn format_ip(ip: IpAddr) -> String {
     let mut ip = match ip {
         IpAddr::V4(v4) => v4.to_string(),
-        IpAddr::V6(v6) => if let Some(v4) = v6.to_ipv4() {
-            v4.to_string()
-        } else {
-            v6.to_string()
+        IpAddr::V6(v6) => {
+            if let Some(v4) = v6.to_ipv4() {
+                v4.to_string()
+            } else {
+                v6.to_string()
+            }
         }
     };
     info!("Request from {}", ip);
@@ -28,15 +29,19 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let make_svc = make_service_fn(|socket: &AddrStream| {
         let remote_addr = socket.remote_addr();
         async move {
-            Ok::<_, Infallible>(service_fn(move |_: Request<Body>| async move {
-                Ok::<_, Infallible>(
-                    Response::new(Body::from(format_ip(remote_addr.ip())))
-                    )
+            Ok::<_, Infallible>(service_fn(move |_: Request<Body>| {
+                async move {
+                    Ok::<_, Infallible>(Response::new(Body::from(format_ip(remote_addr.ip()))))
+                }
             }))
         }
     });
 
-    let port = std::env::args().skip(1).next().map(|x| x.parse().expect("Cannot parse argument as port")).unwrap_or(3000);
+    let port = std::env::args()
+        .skip(1)
+        .next()
+        .map(|x| x.parse().expect("Cannot parse argument as port"))
+        .unwrap_or(3000);
 
     let addr = ([0, 0, 0, 0, 0, 0, 0, 0], port).into();
 
